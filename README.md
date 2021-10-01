d3c85f7fde8d2f8c9d4b9b646294e4263bc677d4
Sarah Drew <drew.sarah.e@gmail.com>
2021-09-30 17:12:03 -0700

    add database for users

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ This project was made during my time at DevMountain to practice React.
 This is the mockup image I was given to design the site. I was also given the data for the users.
 
 ![Mockup Image](https://ed.devmountain.com/materials/webdev6/exercises/user-directory/_images/mockup.jpeg)
+
+## To run:
+
+1. Start server with npx json-server -p 3500 -w database/db.json
+2. npm start
--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import "./styles.css";
 import TopBar from "./components/TopBar";
 import Card from "./components/Card";
-import usersData from "./data";
+import api from "./api/users";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const { data } = await api.get("/users");
+        setUsersData(data);
+      } catch (e) {
+        if (e.response) {
+          console.log(e.response.data);
+          console.log(e.response.status);
+          console.log(e.response.headers);
+        } else {
+          console.log(e.message);
+        }
+      }
+    };
+    getUserData();
+  }, []);
+
   return (
     <>
       <TopBar />
-      <Card usersData={usersData} />
+      {usersData.length > 1 ? (
+        <Card usersData={usersData} />
+      ) : (
+        <div>Loading</div>
+      )}
     </>
   );
 }
